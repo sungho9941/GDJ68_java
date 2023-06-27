5ece96ac887dee2a3bff9e327b8204ad7d45ebd7
Seongho <sungho9941@gmail.com>
2023-06-27 15:53:41 +0900

    test2

---
+++ src/com/karina/study2/Study3.java
@@ -0,0 +1,21 @@
+package com.karina.study2;
+
+public class Study3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=20;
+		
+		switch(num*2) {
+		case 10:
+			System.out.println("num 10");
+			break;
+		case 20:
+			System.out.println("num 20");
+			break;
+		default:
+			System.out.println("num 모름");
+		}
+	}
+
+}
