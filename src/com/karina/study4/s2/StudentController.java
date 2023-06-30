cdc67ae683f6c56d8d35fb2650ab26790577cd1e
Seongho <sungho9941@gmail.com>
2023-06-30 17:52:16 +0900

    0630

---
+++ src/com/karina/study4/s2/StudentController.java
@@ -0,0 +1,9 @@
+package com.karina.study4.s2;
+
+public class StudentController {
+	public void start() {
+//		1. 학생정보 생성 -- makeStudents 호출 
+//		2. 학생정보 출력 -- StudentView에서 출력
+//		3. 프로그램 종료
+	}
+}
---
+++ src/com/karina/study4/s2/StudentView.java
@@ -0,0 +1,5 @@
+package com.karina.study4.s2;
+
+public class StudentView {
+
+}
